2b2a00d4eeb678cfc6139b9b81ea9a09db0b19aa
joshwilcken <joshwilcken@gmail.com>
2017-09-19 12:55:21 -0600

    updated the bottom image

--- src/components/bottomContainer/BottomContainer.js
+++ src/components/bottomContainer/BottomContainer.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react';
-import Carousel from 'react-bootstrap'
 import './BottomContainer.css'
 
 class BottomContainer extends Component {
     render() {
         return (
             <div className='bottomContainer'>
-                <div className='searchContainer'>
+                <div className='search-container'>
+                    <img src={require("../../images/colonelsclub.jpg")} />
+                    <h2>JOIN THE COLONEL'S CLUB</h2>
                     <div className='findStore'>
                         <p>FIND A KFC STORE NEAR YOU</p>
-                    </div>
-                    <div className='locationIconContainer'>
                         <img src={require('../../images/ic_location_on_white_24px.svg')} alt='Location'/>
                     </div>
                 </div>
-                <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
-                    <Carousel.Item>
-                        <img src={require('../../images/slickCarouselImages/girl-with-pillow.jpg')} alt=''/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={require('../../images/slickCarouselImages/WoodsKFC.jpg')} alt=''/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={require('../../images/slickCarouselImages/Sweater.jpg')} alt=''/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={require('../../images/slickCarouselImages/multiple-arms.jpg')} alt=''/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={require('../../images/slickCarouselImages/chicken-Chattin.jpg')} alt=''/>
-                </Carousel.Item>
-            </Carousel>
-        </div>
+            </div>
     );
 }
 }
